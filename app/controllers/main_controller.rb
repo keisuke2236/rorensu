793ef618588ab0bf7549115a9aed67da05797b7d
keisuke2236 <terada@basicinc.jp>
2017-02-16 14:12:51 +0900

    検索できるようになった

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,4 +4,11 @@ class MainController < ApplicationController
     url = "http://image.search.yahoo.co.jp/search?p=" + ERB::Util.url_encode(keyword)
     @images = Scrape.images(url)
   end
+
+  def search
+    keyword = params["word"]
+    url = "http://image.search.yahoo.co.jp/search?p=" + ERB::Util.url_encode(keyword)
+    @images = Scrape.images(url)
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'main/index'
 
   root to: "main#index"
+  post 'search', to: 'main#search' , :as => :search
 end
